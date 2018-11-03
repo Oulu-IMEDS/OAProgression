afd4845b6ba226ab4d705bf8bfc1aa9c4f933fe8
lext <aleksei.tiulpin@protonmail.ch>
2018-11-03 19:06:29 +0200

    Full RS data evaluation

---
+++ oaprogression/evaluation/stats.py
@@ -0,0 +1,71 @@
+import numpy as np
+from scipy import interp
+from sklearn.metrics import roc_auc_score, roc_curve, accuracy_score
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+def roc_curve_bootstrap(model_cls, y, preds, savepath=None, n_bootstrap=1000, seed=42):
+    """Evaluates ROC curve using bootstrapping
+
+    Also reports confidence intervals and prints them.
+
+    Parameters
+    ----------
+    model_cls : object
+        Model to be shown in the report.
+    y : numpy.array
+        Ground truth
+    preds : numpy.array
+        Predictions
+    savepath: str
+        Where to save the figure with ROC curve
+    n_bootstrap:
+        Number of bootstrap samples to draw
+    seed : int
+        Random seed
+
+    """
+    auc = np.round(roc_auc_score(y, preds), 5)
+    np.random.seed(seed)
+    aucs = []
+    tprs = []
+    base_fpr = np.linspace(0, 1, 1001)
+    for _ in tqdm(range(n_bootstrap), total=n_bootstrap):
+        ind = np.random.choice(y.shape[0], y.shape[0])
+        if y[ind].sum() == 0:
+            continue
+        aucs.append(roc_auc_score(y[ind], preds[ind]))
+        fpr, tpr, _ = roc_curve(y[ind], preds[ind])
+        tpr = interp(base_fpr, fpr, tpr)
+        tpr[0] = 0.0
+        tprs.append(tpr)
+
+    tprs = np.array(tprs)
+    mean_tprs = np.mean(tprs, 0)
+    std = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tprs + std, 1)
+    tprs_lower = mean_tprs - std
+
+    print('Model:', model_cls)
+    plt.figure(figsize=(6, 6))
+    plt.fill_between(base_fpr, tprs_lower, tprs_upper, color='grey', alpha=0.2)
+    plt.plot(base_fpr, mean_tprs, 'r-')
+    plt.plot([0, 1], [0, 1], '--', color='black')
+
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.grid()
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.tight_layout()
+    if savepath is not None:
+        plt.savefig(savepath, bbox_inches='tight')
+    plt.show()
+
+    CI_l, CI_h = np.percentile(aucs, 2.5), np.percentile(aucs, 97.5)
+    print('AUC:', auc)
+    print(f'CI [{CI_l:.5f}, {CI_h:.5f}]')
+    return auc, CI_l, CI_h
+
+
--- run_evaluation.py
+++ run_evaluation.py
@@ -1,4 +1,3 @@
-import glob
 import os
 import gc
 import argparse
@@ -9,8 +8,7 @@ import numpy as np
 import cv2
 
 
-
-from oaprogression.evaluation import rstools
+from oaprogression.evaluation import rstools, stats
 
 
 cv2.ocl.setUseOpenCL(False)
@@ -24,6 +22,8 @@ if __name__ == "__main__":
     parser.add_argument('--bs', type=int, default=32)
     parser.add_argument('--n_threads', type=int, default=12)
     parser.add_argument('--snapshots_root', default='/data/DL_spring2/OA_progression_project/snapshots')
+    parser.add_argument('--from_cache', default=False)
+    parser.add_argument('--n_bootstrap', type=int, default=10000)
     parser.add_argument('--snapshot', default='2018_11_03_10_38')
     parser.add_argument('--save_dir', default='/data/DL_spring2/OA_progression_project/Results')
     args = parser.parse_args()
@@ -39,31 +39,59 @@ if __name__ == "__main__":
 
     gradcam_maps_all = 0
     res = 0
-    for fold_id in range(session_snapshot['args'][0].n_folds):
-        features, fc = rstools.init_fold(fold_id, session_snapshot, args)
+    if not args.from_cache:
+        for fold_id in range(session_snapshot['args'][0].n_folds):
+            features, fc = rstools.init_fold(fold_id, session_snapshot, args)
 
-        preds = []
-        gradcam_maps_fold = []
-        ids = []
-        sides = []
-        for batch_id, sample in enumerate(tqdm(loader, total=len(loader), desc='Prediction from fold {}'.format(fold_id))):
-            gcam_batch, probs_not_summed = rstools.eval_batch(sample, features, fc)
-            gradcam_maps_fold.append(gcam_batch)
-            preds.append(probs_not_summed)
-            ids.extend(sample['ergoid'].numpy().tolist())
-            sides.extend(sample['ergoid'])
+            preds = []
+            gradcam_maps_fold = []
+            ids = []
+            sides = []
+            for batch_id, sample in enumerate(tqdm(loader, total=len(loader), desc='Prediction from fold {}'.format(fold_id))):
+                gcam_batch, probs_not_summed = rstools.eval_batch(sample, features, fc)
+                gradcam_maps_fold.append(gcam_batch)
+                preds.append(probs_not_summed)
+                ids.extend(sample['ergoid'].numpy().tolist())
+                sides.extend(sample['side'])
+                gc.collect()
+
+            preds = np.vstack(preds)
+            gradcam_maps_all += np.vstack(gradcam_maps_fold)
+            res += preds
             gc.collect()
 
-        preds = np.vstack(preds)
-        gradcam_maps_all += np.vstack(gradcam_maps_fold)
-        res += preds
-        gc.collect()
+        res /= 5.
+        np.savez_compressed(os.path.join(args.save_dir, f'RS{args.rs_cohort}.npz'),
+                            gradcam_maps_all=gradcam_maps_all,
+                            preds=res,
+                            ids=ids,
+                            sides=sides)
 
-    np.savez_compressed(os.path.join(args.save_dir, f'RS{args.rs_cohort}.npz'),
-                        gradcam_maps_all=gradcam_maps_all,
-                        preds=res,
-                        ids=ids,
-                        sides=sides)
+    data = np.load(os.path.join(args.save_dir, f'RS{args.rs_cohort}.npz'))
+
+    preds = data['preds'][:, 1:].sum(1)
+    ergoids = data['ids']
+    sides = data['sides']
+
+    res = pd.DataFrame(data={'ergoid': ergoids, 'side': sides, 'pred':preds})
+
+    res = pd.merge(rs_meta, res, on=('ergoid', 'side'))
+
+    print('# subjects', np.unique(res.ergoid).shape[0])
+    print('# knees', res.shape[0])
+    print('# progressors', res[res.progressor == 1].shape[0])
+    print('# non-progressors', res[res.progressor == 0].shape[0])
+
+    stats.roc_curve_bootstrap('5-fold CV ensemble with TTA',
+                              res.progressor,
+                              res.pred,
+                              n_bootstrap=args.n_bootstrap)
+
+
+
+
+
+
 
 
 

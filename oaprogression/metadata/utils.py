306e187b95d58529243e03865d331af15c72b055
lext <aleksei.tiulpin@protonmail.ch>
2018-11-21 14:19:00 +0200

    initial version of dataset stats script

--- oaprogression/metadata/utils.py
+++ oaprogression/metadata/utils.py
@@ -1,6 +1,6 @@
 from sas7bdat import SAS7BDAT
 import pandas as pd
-
+import numpy as np
 
 def read_sas7bdata_pd(fname):
     data = []
@@ -9,3 +9,15 @@ def read_sas7bdata_pd(fname):
             data.append(row)
 
     return pd.DataFrame(data[1:], columns=data[0])
+
+def data_stats(dataset):
+    print('## Unique subjects', np.unique(dataset.ID).shape[0])
+    print('## Knees', dataset.ID.shape[0])
+    print('## Knees (left)', dataset[dataset.Side == 'L'].ID.shape[0])
+    print('## Knees (right)', dataset[dataset.Side == 'R'].ID.shape[0])
+    print('## Knees non-progressors', (dataset.Progressor.values == 0).sum())
+    print('## Knees progressors', (dataset.Progressor.values > 0).sum())
+    for KL in [0, 1, 2, 3, 4]:
+        print(" ")
+        print(f'### [{KL}] # knees non-progressors', (dataset[dataset.KL == KL].Progressor.values == 0).sum())
+        print(f'### [{KL}] # knees progressors', (dataset[dataset.KL == KL].Progressor.values > 0).sum())
--- prepare_metadata.py
+++ prepare_metadata.py
@@ -2,12 +2,14 @@ import argparse
 import os
 from oaprogression.metadata import oai
 from oaprogression.metadata import most
+from oaprogression.metadata.utils import data_stats
+import pandas as pd
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--oai_meta', default='')
-    parser.add_argument('--most_meta', default='')
-    parser.add_argument('--save_meta', default='Metadata/')
+    parser.add_argument('--oai_meta', default='/media/lext/FAST/OA_progression_project/Data/X-Ray_Image_Assessments_SAS')
+    parser.add_argument('--most_meta', default='/media/lext/FAST/OA_progression_project/Data/most_meta')
+    parser.add_argument('--save_meta', default='/media/lext/FAST/OA_progression_project/workdir/Metadata/')
     args = parser.parse_args()
 
     os.makedirs(args.save_meta, exist_ok=True)
@@ -15,22 +17,35 @@ if __name__ == "__main__":
         oai_meta = oai.build_img_progression_meta(args.oai_meta)
         oai_meta.to_csv(os.path.join(args.save_meta, 'OAI_progression.csv'), index=None)
     else:
+        oai_meta = pd.read_csv(os.path.join(args.save_meta, 'OAI_progression.csv'))
         print('OAI progression metadata exists!')
 
     if not os.path.isfile(os.path.join(args.save_meta, 'OAI_participants.csv')):
         oai_participants = oai.build_clinical(args.oai_meta)
         oai_participants.to_csv(os.path.join(args.save_meta, 'OAI_participants.csv'), index=None)
     else:
+        oai_participants = pd.read_csv(os.path.join(args.save_meta, 'OAI_participants.csv'))
         print('OAI participants metadata exists!')
 
     if not os.path.isfile(os.path.join(args.save_meta, 'MOST_progression.csv')):
         most_meta = most.build_img_progression_meta(args.most_meta)
         most_meta.to_csv(os.path.join(args.save_meta, 'MOST_progression.csv'), index=None)
     else:
+        most_meta = pd.read_csv(os.path.join(args.save_meta, 'MOST_progression.csv'))
         print('MOST progression metadata exists!')
 
     if not os.path.isfile(os.path.join(args.save_meta, 'MOST_participants.csv')):
-        most_meta = most.build_clinical(args.most_meta)
-        most_meta.to_csv(os.path.join(args.save_meta, 'MOST_participants.csv'), index=None)
+        most_participants = most.build_clinical(args.most_meta)
+        most_participants.to_csv(os.path.join(args.save_meta, 'MOST_participants.csv'), index=None)
     else:
+        most_participants = pd.read_csv(os.path.join(args.save_meta, 'MOST_participants.csv'))
         print('MOST participants metadata exists!')
+
+    print(" ")
+    print("# ======== OAI ======== ")
+    data_stats(oai_meta)
+    print(" ")
+    print("# ======== MOST ======== ")
+    data_stats(most_meta)
+
+    

4820c4713172dad863a01cfde325afa73e95ddd0
lext <aleksei.tiulpin@protonmail.ch>
2018-12-08 14:40:40 +0200

    baseline trainign refactoring

--- oaprogression/training/baselines.py
+++ oaprogression/training/baselines.py
@@ -2,14 +2,14 @@ import pickle
 import argparse
 import os
 import pandas as pd
-
+import numpy as np
+from sklearn.linear_model import LogisticRegression
 
 def init_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--metadata_root', default='')
     parser.add_argument('--snapshots_root', default='')
     parser.add_argument('--snapshot', default='')
-    parser.add_argument('--seed', type=int, default=123456)
     parser.add_argument('--save_dir', default='')
     parser.add_argument('--n_bootstrap', type=int, default=2000)
     parser.add_argument('--n_vals_c', type=int, default=100)
@@ -39,5 +39,55 @@ def init_metadata(args):
 
         train_folds.append((train_split, val_split))
 
-    return train_folds, metadata_test
+    return train_folds, metadata_test, session_snapshot['args'][0].seed
 
+def build_logreg_model(train_folds, feature_set, seed, n_vals_c, metric):
+    cv_scores = []
+    models = []
+    means_stds = []
+    c_vals = np.logspace(-6, 0, n_vals_c)
+    for C in c_vals:  # Enumerating the regularizer weight
+        folds_predicts = []
+        folds_gt = []
+        folds_models = []
+        folds_means_stds = []
+        for fold_id, (train_split, val_split) in enumerate(train_folds):  # Going through the prepared fold splits
+            X_train = train_split[feature_set].values.astype(float)
+            X_val = val_split[feature_set].values.astype(float)
+            
+            y_train = train_split.Progressor.values > 0
+            y_val = val_split.Progressor.values > 0
+            
+            y_train = y_train[~np.isnan(X_train).any(1)]
+            X_train = X_train[~np.isnan(X_train).any(1)]
+            
+            y_val = y_val[~np.isnan(X_val).any(1)]
+            X_val = X_val[~np.isnan(X_val).any(1)]
+            
+            mean = np.mean(X_train, 0)
+            std = np.std(X_train, 0)
+            
+            X_train -= mean
+            X_train /= std
+            
+            X_val -= mean
+            X_val /= std
+
+            clf = LogisticRegression(C=C, random_state=seed, solver='lbfgs')
+            clf.fit(X_train, y_train)
+            p_val = clf.predict_proba(X_val)[:, 1]
+
+            folds_predicts.extend(p_val.flatten().tolist())
+            folds_gt.extend(y_val.flatten().tolist())
+            folds_models.append(clf)
+            folds_means_stds.append([mean, std])
+
+        auc = metric(folds_gt, folds_predicts)
+        cv_scores.append(auc)
+        models.append(folds_models)
+        means_stds.append(folds_means_stds)
+
+    opt_c_id = np.argmax(cv_scores)
+    models_best = models[opt_c_id]
+    mean_std_best = means_stds[opt_c_id]
+    return models_best, mean_std_best, cv_scores[opt_c_id]
--- run_baselines.py
+++ run_baselines.py
@@ -1,73 +1,30 @@
 import os
+if int(os.getenv('USE_AGG', 0)) == 1:
+    import matplotlib
+    matplotlib.use('Agg')
+
 import numpy as np
 import pickle
+
 import matplotlib.pyplot as plt
 
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, average_precision_score
 
 from oaprogression.training import baselines
 from oaprogression.evaluation import stats
 
 if __name__ == "__main__":
     args = baselines.init_args()
-    train_folds, metadata_test = baselines.init_metadata(args)
+    train_folds, metadata_test, seed = baselines.init_metadata(args)
     os.makedirs(args.save_dir, exist_ok=True)
 
-    c_vals = np.logspace(-6, 0, args.n_vals_c)
     results = {}
     for feature_set in [['AGE', 'SEX', 'BMI'],
+                        ['KL',],
                         ['AGE', 'SEX', 'BMI', 'KL'],
                         ['AGE', 'SEX', 'BMI', 'KL', 'SURG', 'INJ', 'WOMAC']]:
-        cv_scores = []
-        models = []
-        means_stds = []
-        for C in c_vals:
-            folds_predicts = []
-            folds_gt = []
-            folds_models = []
-            folds_means_stds = []
-            for fold_id, (train_split, val_split) in enumerate(train_folds):
-                X_train = train_split[feature_set].values.astype(float)
-                X_val = val_split[feature_set].values.astype(float)
-
-                y_train = train_split.Progressor.values > 0
-                y_val = val_split.Progressor.values > 0
-
-                y_train = y_train[~np.isnan(X_train).any(1)]
-                X_train = X_train[~np.isnan(X_train).any(1)]
-
-                y_val = y_val[~np.isnan(X_val).any(1)]
-                X_val = X_val[~np.isnan(X_val).any(1)]
-
-                mean = np.mean(X_train, 0)
-                std = np.std(X_train, 0)
-
-                X_train -= mean
-                X_train /= std
-
-                X_val -= mean
-                X_val /= std
-
-                clf = LogisticRegression(C=C, random_state=args.seed, solver='lbfgs')
-                clf.fit(X_train, y_train)
-                p_val = clf.predict_proba(X_val)[:, 1]
-
-                folds_predicts.extend(p_val.flatten().tolist())
-                folds_gt.extend(y_val.flatten().tolist())
-                folds_models.append(clf)
-                folds_means_stds.append([mean, std])
-
-            auc = roc_auc_score(folds_gt, folds_predicts)
-            cv_scores.append(auc)
-            models.append(folds_models)
-            means_stds.append(folds_means_stds)
-
-        opt_c_id = np.argmax(cv_scores)
-        models_best = models[opt_c_id]
-        mean_std_best = means_stds[opt_c_id]
-
-        print('CV score:', feature_set, c_vals[opt_c_id], cv_scores[opt_c_id])
+        models_best, mean_std_best, cv_scores = baselines.build_logreg_model(train_folds, feature_set, seed, args.n_vals_c, average_precision_score)
+        print('CV score:', feature_set, cv_scores)
 
         y_test = metadata_test.Progressor.values.copy() > 0
         ids = metadata_test.ID.values
